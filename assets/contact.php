d342e112432982694a96f80264ca0e529554f1af
Danmark12 <danmarkpetalcurin@gmail.com>
2026-01-14 00:35:26 +0800

    added map for contacts

--- assets/contact.php
+++ assets/contact.php
@@ -175,7 +175,14 @@
     </a>
   </div>
 
-
+  <!-- Map (Right) -->
+  <div class="map-container">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.227712789576!2d124.8637!3d8.3667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32661eb84772227d%3A0xdee701d8c2b7b102!2sManolo%20Fortich%2C%20Bukidnon!5e0!3m2!1sen!2sph!4v1700000000000!5m2!1sen!2sph"
+      allowfullscreen=""
+      loading="lazy">
+    </iframe>
+  </div>
 </section>
 
 <!-- Footer -->
